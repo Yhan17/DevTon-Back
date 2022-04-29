f9f0a99b468f863a791f3a743a26db8b2b3aa866
Yhan.Nunes <yhan.silva@tonolucro.com.br>
2022-04-28 22:55:54 -0300

    ✨Add dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import http from 'http'
+import { Server, Socket } from "socket.io";
 
 const app = express()
 const PORT = 3333
 app.use(express.json())
 app.use(cors())
+const httpServer = http.createServer(app)
+const io = new Server(httpServer)
 
+app.get('/', (req,res) => {
+  return res.json({message: "running"})
+})
+
+app.listen(PORT, () => console.log(`🎉 App Running on ${PORT}`))
